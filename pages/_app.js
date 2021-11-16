9b493f776965e039b1a3a9b8eda668e7a16cd89d
bagus tri harjanto <bagustri15@gmail.com>
2021-11-16 13:10:20 +0700

    add styling import css / scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import "styles/globals.css";
+// import "styles/globals.css";
+import "styles/global.scss";
 import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
